e54ff520d220c27950562aa34b6c7521a96616e6
Yatharth Thaker <52533890+thaker0503@users.noreply.github.com>
2022-08-09 15:09:01 +0530

    pp

--- script.js
+++ script.js
@@ -1,24 +1,67 @@
-window.onload = function() {
+window.onload = function () {
     document.getElementById("txt").focus();
 }
+$(document).keypress(function (e) {
+    if (e.which == 13) {
+        $("#btn").click();
+    }
+});
 
-  function addItem() {
-    if(document.querySelector('#txt').value != ""){
-    document.querySelector('.lii').innerHTML +=
-        `<div class='list'> 
-    ${document.querySelector('#txt').value}  <i class="fa-solid fa-xmark close"></i>`
 
-    document.querySelector('#txt').value = "";
-    }    
-    else{
+
+function addItem() {
+    if (document.querySelector('#txt').value != "") {
+        document.querySelector('.lii').innerHTML +=
+            `<div class='list'> 
+        
+        <input type="checkbox" id="myCheckbox" onclick="checkBoxClick()" class="checkBox"/> 
+        <span class="todo-description"> 
+            <span class="title"> ${document.querySelector('#txt').value}</span> 
+            <span id="displayDate">${new Date().toLocaleString()}</span> 
+        </span>  
+        <i id="del" class="fa-solid fa-trash-can fa-xs close"></i> `;
+
+        document.querySelector('#txt').value = "";
+    }
+    else {
         alert("Enter something to Add!");
         document.getElementById('#txt').focus();
-    }    
+    }
 
     var current_tasks = document.querySelectorAll(".close");
     for (var i = 0; i < current_tasks.length; i++) {
         current_tasks[i].onclick = function () {
             this.parentNode.remove();
         }
+    }
+
+
+
+
+
+    // for (var i=0; i<checkbox.length; i++){
+    //     checkbox[i].onclick = function() {
+    //         title[i].classList.toggle('checked');
+    //     }
+    // }
+
+    // Add a "checked" symbol when clicking on a list item
+    // var checkbox = document.querySelector('.checkBox');
+    // checkbox.addEventListener('click', function(ev) {
+    // if (ev.target. === 'checkBox') {
+    //     ev.target.classList.toggle('checked');
+    // }
+    // }, false);
+
+}
+
+function checkBoxClick() {
+    var checkbox = document.querySelectorAll("#myCheckbox");
+    var title = document.querySelectorAll(".title");
+    for (let i = 0; i < checkbox.length; i++) {
+        if (checkbox[i].checked != false) {
+            title[i].classList.toggle("checked");
+        }
+        title[i].classList("");
     }
 }
